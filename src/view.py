fe562d6fd0224acb66a3b0d303755a20bc0a5c76
Aryan Jain <JainAryan@hotmail.co.uk>
2023-03-01 19:39:17 +0000

    added sorting functionality for tables

--- src/view.py
+++ src/view.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import os
 from flask_mysqldb import MySQL
 
@@ -15,12 +15,51 @@ mysql = MySQL(app)
 def home():
     return render_template('index.html')
 
-@app.route('/movies')
+@app.route('/movies', methods=['GET', 'POST'])
 def movies():
     cur = mysql.connection.cursor()
+    name1 = request.form.get('vehicle1')
+    name2 = request.form.get('vehicle2')
+    select = request.form.get('options')
+    
+    value=""
     cur.execute("SELECT * FROM movies")
     value = cur.fetchall()
-    cur.close()
+    # cur.close()
+    if name1 and select == "movieId":
+        cur.execute("SELECT * FROM movies ORDER BY movieId ASC")
+        value = cur.fetchall()
+        # cur.close()
+    if name2 and select == "movieId":
+        cur.execute("SELECT * FROM movies ORDER BY movieId DESC")
+        value = cur.fetchall()
+        # cur.close()
+    if name1 and select == "title":
+        cur.execute("SELECT * FROM movies ORDER BY title ASC")
+        value = cur.fetchall()
+        # cur.close()
+    if name2 and select == "title":
+        cur.execute("SELECT * FROM movies ORDER BY title DESC")
+        value = cur.fetchall()
+        # cur.close()
+    if name1 and select == "year":
+        cur.execute("SELECT * FROM movies ORDER BY year ASC")
+        value = cur.fetchall()
+        # cur.close()
+    if name2 and select == "year":
+        cur.execute("SELECT * FROM movies ORDER BY year DESC")
+        value = cur.fetchall()
+        # cur.close()
+    # name=request.form['name'] 
+    # email=request.form['email'] 
+    # password2=request.form['password2'] 
+    # birthday=request.form['birthday']
+    # cur.execute("insert into table_name values (%s,%s,%s,%s)",       (name,email,password2,birthday)) 
+    # con.commit() 
+    
+    # cur.execute("SELECT * FROM movies")
+    # value = cur.fetchall()
+    # cur.close()
     return render_template('movies.html', data=value, name='Movies')
 
 @app.route('/genres')
